fd834a7363d8e69a6fc220439b341475a05f11c1
Dhananjay Nene <dhananjay.nene@gmail.com>
2013-08-15 12:07:33 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+requires = [
+    "beautifulsoup4==4.2.1",
+    "requests==1.2.3",
+    "mechanize==0.2.5",
+]
+
+setup(name='pykindle',
+      version='1.0',
+      description='Python Kindle scraper',
+      author='Dhananjay Nene',
+      author_email='dhananjay.nene@gmail.com',
+      url='http://github.com/dnene/pykindle',
+      packages=['pykindle'],
+      install_requires = requires,
+     )
